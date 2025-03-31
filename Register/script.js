93631165509386a73feebe2fa887ba0de144da3c
Kauã Crescêncio <vigorcei@gmail.com>
2025-03-31 12:44:05 -0300

    Add files via upload

---
+++ Register/script.js
@@ -0,0 +1,12 @@
+let letmudaresquemasenha1 = 0;
+
+let letmudaresquemasenha2 = 0;
+
+const BntShowPassWoed1 = document.getElementById("icon-show-password1");
+
+const BntShowPassWoed2 = document.getElementById("icon-show-password2");
+
+const input1 = document.getElementById('input1');
+
+const input2 = document.getElementById('input2');
+
